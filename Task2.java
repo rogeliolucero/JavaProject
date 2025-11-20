053b0cd5ac683c38890500b312d667ee4b3b8547
rogeliolucero <128567187+rogeliolucero@users.noreply.github.com>
2025-11-20 06:25:36 +0000

    perform task2

---
+++ Task2.java
@@ -0,0 +1,22 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int result = 1;
+        result += 2;
+        System.out.println(result);
+
+        result -= 1;
+        System.out.println(result);
+
+        result *= 2;
+        System.out.println(result);
+
+        result /= 2;
+        System.out.println(result);
+
+        result += 8;
+        result %= 7;
+        System.out.println(result);
+    }
+}
